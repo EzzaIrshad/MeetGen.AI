58de86e07efda0398be2c1c74f7cc84377fbba45
Ezza Irshad <ezzairshad19@gmail.com>
2026-01-27 01:30:01 +0500

    introduced edit and delete agents functionality

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,15 +1,53 @@
 import { createTRPCRouter, protectedProcedure } from "@/app/trpc/init";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { agentsInsertSchema } from "../schemas";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schemas";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
-    getOne: protectedProcedure.input(z.object({ id: z.string() }))
-    .query(async ({ input, ctx }) => {
+
+    // update item procedure
+    update: protectedProcedure.input(agentsUpdateSchema)
+        .mutation(async ({ input, ctx }) => {
+            const [updatedAgent] = await db.update(agents)
+                .set(input)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+            if (!updatedAgent) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+            }
+            return updatedAgent;
+        }),
+
+    // remove item procedure
+    remove: protectedProcedure.input(z.object({ id: z.string() }))
+        .mutation(async ({ input, ctx }) => {
+            const [removedAgent] = await db.delete(agents)
+                .where(
+                    and(
+                        eq(agents.id, input.id),
+                        eq(agents.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+            if (!removedAgent) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+            }
+            return removedAgent;
+        }),
+
+    // Get one item procedure
+    getOne: protectedProcedure.input(
+        z.object({ id: z.string() })
+    ).query(async ({ input, ctx }) => {
         const [existingAgent] = await db.select({
             ...getTableColumns(agents),
             // TODO: Replace with actual meeting count
@@ -23,12 +61,14 @@ export const agentsRouter = createTRPCRouter({
                 )
             );
 
-            if(!existingAgent){
-                throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
-            }
+        if (!existingAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+        }
 
         return existingAgent
     }),
+
+    //get many items procedure
     getMany: protectedProcedure.input(
         z.object({
             page: z.number().default(DEFAULT_PAGE),
@@ -72,6 +112,8 @@ export const agentsRouter = createTRPCRouter({
                 totalPages,
             }
         }),
+
+    // create item procedure
     create: protectedProcedure.input(agentsInsertSchema)
         .mutation(async ({ input, ctx }) => {
             const [createdAgent] = await db.insert(agents)
